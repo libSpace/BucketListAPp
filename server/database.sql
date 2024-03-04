f855607764df2bb4ddd0452a08d1051489605ba3
libSpace <malikmphomalope@gmail.com>
2024-03-04 13:02:49 +0200

    Created another database and another table

--- server/database.sql
+++ server/database.sql
@@ -1,6 +1,9 @@
-CREATE DATATBASE bucketlist;
+CREATE DATABASE busket_buddies; --This SQL statement creates a new PostgreSQL database named "busket_buddies."
 
-CREATE TABLE item(
-    item_id SERIAL PRIMARY KEY,
-    type VARCHAR(255),
+CREATE TABLE grocery_list(
+    groc_id SERIAL PRIMARY KEY, --used to be item_id
+    item_name VARCHAR(255), --used to be type
+    description VARCHAR(255), 
+    quantity INT,
+    price NUMERIC(10,2)
 );
