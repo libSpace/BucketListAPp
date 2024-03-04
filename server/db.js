a6da56933aa2a481e636fa77a06d9446c2c8da1a
libSpace <malikmphomalope@gmail.com>
2024-03-04 13:02:23 +0200

    Changed the previous databse name

--- server/db.js
+++ server/db.js
@@ -1,12 +1,23 @@
-//connects to our database
+// Import PostgreSQL connection pool
 const Pool = require("pg").Pool;
 
+// Create a new connection pool configuration
 const pool = new Pool({
+    // Database user
     user: "postgres",
+    
+    // User password
     password: "94072",
+    
+    // Database host (localhost in this case)
     host: "localhost",
+    
+    // Port on which the PostgreSQL server is running (default is 5432)
     port: 5432,
-    database: "bucketlist"
+    
+    // Database name
+    database: "busket_buddies"
 });
 
+// Export the created connection pool for external use
 module.exports = pool;
